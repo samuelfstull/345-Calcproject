fcaa0183c775d3ceb0fec866a54e2b5c4077810a
Sam Stull <sfstull@bsu.edu>
2018-02-28 20:55:09 -0500

    initial commit

---
+++ src/edu/bsu/cs345/project1/Controller.java
@@ -0,0 +1,9 @@
+package edu.bsu.cs345.project1;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void CalculateGrade(ActionEvent actionEvent) {
+        System.out.println("test");
+    }
+}
