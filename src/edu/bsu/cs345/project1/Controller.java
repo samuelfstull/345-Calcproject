23bcbe900c03eb6e3c147064126dd44e4a3227c3
Sam Stull <sfstull@bsu.edu>
2018-03-01 21:31:19 -0500

    final commit

--- src/edu/bsu/cs345/project1/Controller.java
+++ src/edu/bsu/cs345/project1/Controller.java
@@ -1,33 +1,40 @@
 package edu.bsu.cs345.project1;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Controller {
     public TextArea outputfield;
     public TextField gradefield;
-    ArrayList<Integer> percents = new ArrayList<>();
-    @SuppressWarnings("unused")
-    private String finalText;
-    int finalpercent=0;
+    private ArrayList<Integer> percents = new ArrayList<>();
+    private int finalpercent=0;
 
+    @SuppressWarnings({"unused", "ForLoopReplaceableByForEach"})//for required and unused for action event
     public void CalculateGrade(ActionEvent actionEvent) {
-            String text=gradefield.getText();
+        try {
+            ArrayList<Integer> integers = stringToArrayList(gradefield);
 
-            text.replaceAll("\\s","");
-           String[] eachAssign = text.split(",");
-           //Integer.parseInt(myString)
-
-        outputfield.setText(finalText);
+            percents.clear();
+            finalpercent = 0;
+            for (int x = 0; x < integers.size(); x++) {
+                AddPercents(integers.get(x));
+            }
+            SummingPercents();
+            outputfield.setText(Integer.toString(finalpercent) + "%");
+        }
+        catch (Exception e){
+            outputfield.setText("error");
+        }
     }
 
-    public void AddPercents (int grade){
+    private void AddPercents (int grade){
         if (grade==0){
-            percents.add(0);
+            percents.add(45);
         }
         else if (grade==1){
             percents.add(60);
@@ -38,12 +45,40 @@ public class Controller {
         else if (grade==3){
             percents.add(100);
         }
-    }
-    public void SummingPercents(){
 
-        for (int i=0; i<percents.size();i++){
+    }
+    @SuppressWarnings("ForLoopReplaceableByForEach")//the value of i is needed
+    private void SummingPercents(){
+        int size=percents.size();
+        for (int i=0; i<size;i++){
                     finalpercent= finalpercent+percents.get(i);
         }
+        finalpercent= finalpercent/size;
+    }
+    private ArrayList<Integer> stringToArrayList(TextField textField){
+        ArrayList<Integer> tokens = new ArrayList<>();
+
+        try{
+            String userInput = textField.getText();
+            Scanner scanner = new Scanner(userInput);
+            scanner.useDelimiter("[,]");
+
+            while (scanner.hasNext()){
+                String string = scanner.next();
+                string = string.replace(" ", "");
+                string = string.replace("{", "");
+                string = string.replace("}", "");
+                string = string.replace(" ", "");
+                string = string.replaceAll("\\p{P}", "");
+                int integer = Integer.parseInt(string);
+                tokens.add(integer);
+            }
+        }
+        catch (Exception e){
+            //MainViewController.infoBox("Invalid input", "NOTICE!");
+            return tokens;
+        }
+        return tokens;
     }
 
 
